5a852560db0500012711d390307ad4ef4f873b9c
obitech <obitech@users.noreply.github.com>
2018-12-10 21:06:21 +0100

    added redis-address commandline flag

--- cmd/item/root.go
+++ cmd/item/root.go
@@ -26,4 +26,5 @@ func init() {
 	f.StringVarP(&address, "address", "a", address, "listening address")
 	f.StringVarP(&endpoint, "endpoint", "e", endpoint, "endpoint for other services to reach item service")
 	f.StringVarP(&logLevel, "log-level", "l", logLevel, "log level (debug, info, warn, error), empty or invalid values will fallback to default")
+	f.StringVarP(&redis, "redis-address", "r", redis, "redis address to connect to")
 }
