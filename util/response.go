6e86d79d0917db16c2b5da802d0fe07711cdd7be
obitech <obitech@users.noreply.github.com>
2018-12-13 11:29:24 +0100

    response tests

--- util/response.go
+++ util/response.go
@@ -15,20 +15,22 @@ type Response struct {
 
 // NewResponse returns a Response with a passed message string and slice of Data.
 // This will automatically set the Status field to 200.
-func NewResponse(s int, m string, c int, d interface{}) Response {
+func NewResponse(s int, m string, c int, d interface{}) (Response, error) {
 	return Response{
 		Status:  s,
 		Message: m,
 		Count:   c,
 		Data:    d,
-	}
+	}, nil
 }
 
 // SendJSON encodes a Response as JSON and sends it on a passed http.ResponseWriter.
-func (r Response) SendJSON(w http.ResponseWriter) {
+func (r Response) SendJSON(w http.ResponseWriter) error {
 	w.Header().Set("Content-Type", "application/JSON; charset=UTF-8")
 	w.WriteHeader(r.Status)
-	if err := json.NewEncoder(w).Encode(r); err != nil {
+	err := json.NewEncoder(w).Encode(r)
+	if err != nil {
+
 		status := http.StatusInternalServerError
 		w.WriteHeader(status)
 
@@ -38,9 +40,5 @@ func (r Response) SendJSON(w http.ResponseWriter) {
 
 		_ = json.NewEncoder(w).Encode(r)
 	}
-}
-
-// SendErrorJSON uses SendJSON to send a passed error code and message
-func SendErrorJSON(w http.ResponseWriter, s int, m string) {
-	NewResponse(s, m, 0, nil).SendJSON(w)
+	return err
 }
